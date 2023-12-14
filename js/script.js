884a192a492fcec2cfa44e92706b12af2711407b
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-12-14 16:49:16 +0100

    add: disk highlighted

--- js/script.js
+++ js/script.js
@@ -5,11 +5,19 @@ createApp({
         return {
             diskList: [],
             apiUrl: "server.php",
+            diskHighlighted: null,
         };
     },
     created() {
         axios.get(this.apiUrl).then((resp) => {
             this.diskList = resp.data;
         });
+    },
+    methods: {
+        clickedDisk(id) {
+            axios.get(this.apiUrl).then((resp) => {
+                this.diskHighlighted = resp.data[id];
+            });
+        }
     },
 }).mount("#app");
